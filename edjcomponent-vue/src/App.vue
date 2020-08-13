717dd093ed72cfbe466edbfff01cefe589353620
张永杰 <zhangyongjie@edaijia-inc.cn>
2020-08-13 19:29:23 +0800

    cs

--- edjcomponent-vue/src/App.vue
+++ edjcomponent-vue/src/App.vue
@@ -1,13 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    aaa
+    <cus-input :value="text" @input="input">111</cus-input>
   </div>
 </template>
-
+<script>
+import CusInput from "./components/login/login";
+export default {
+  data(){
+    return {
+      text: ""
+    }
+  },
+  created(){
+    console.log(this)
+  },
+  components: {
+    CusInput
+  },
+  methods:{
+    input(e){
+      this.text = e;
+      console.log(this.text)
+    }
+  }
+}
+</script>
 <style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ edjcomponent-vue/src/components/login/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <input :value="value" @input="$emit('input', $event.target.value)" type="text">
+</template>
+<script>
+export default {
+  props: ["value"],
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {}
+};
+</script>
+
+<style scoped></style>
