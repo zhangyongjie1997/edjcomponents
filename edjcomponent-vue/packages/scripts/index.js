f9646a1f810e3c63f8657541f383b03a7dc3acad
张永杰 <zhangyongjie@edaijia-inc.cn>
2020-08-31 10:59:50 +0800

    cs

--- edjcomponent-vue/packages/scripts/index.js
+++ edjcomponent-vue/packages/scripts/index.js
@@ -5,3 +5,28 @@ export default {
   utils,
   request
 };
+
+function Promise(fn) {
+  this.cbs = [];
+  this.data = undefined;
+  const resolve = value => {
+    setTimeout(() => {
+      this.data = value;
+      this.cbs.forEach(cb => cb(value));
+    }, 0);
+  };
+  fn(resolve.bind(this));
+}
+
+Promise.prototype.then = function(onResolved) {
+  return new Promise(resolve => {
+    this.cbs.push(() => {
+      const res = onResolved(this.data);
+      if (res instanceof Promise) {
+        res.then(resolve);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+};
